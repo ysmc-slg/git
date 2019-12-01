d4ddf310ea6b89b5afd0391969a8b976f3340ccb
zxq <494786209@qq.com>
2019-12-01 17:02:15 +0800

    修改

--- git_test/src/com/itheima/git/Test.java
+++ git_test/src/com/itheima/git/Test.java
@@ -2,6 +2,9 @@ package com.itheima.git;
 
 public class Test {
     public static void main(String[] args) {
+
+        System.out.println("hello git");
+        System.out.println("hello git");
         System.out.println("hello git");
     }
 }
